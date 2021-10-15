b03ac70b9ce29fe41319d91b66ea56c5f4b1a067
Colin Monaghan <cleemonaghan@gmail.com>
2021-10-15 16:15:46 -0700

    Specified error type in showPassengers.php

--- proj2/showPassengers.php
+++ proj2/showPassengers.php
@@ -39,10 +39,14 @@
                 echo "<br>";
             }
             if(isset($_GET['error'])) { // if ssn being updated was invalid
-                $message = "No passenger with inputed SSN was found! Please reselect.";
-                echo "$message";
-                echo "<br>";
-                echo "<br>";
+                $errorType = $_GET['error'];
+                if($errorType == '1'){
+                    $message = "No passenger with inputed SSN was found! Please reselect.";
+                    echo "$message";
+                    echo "<br>";
+                    echo "<br>";
+                }
+
             }​
         ?>
 
