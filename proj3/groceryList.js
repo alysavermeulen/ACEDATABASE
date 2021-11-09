c899be4ecb56e081e105090d1a937ad7dbc0e028
Colin Monaghan <cleemonaghan@gmail.com>
2021-11-09 15:39:04 -0800

    Updated groceryList.js and groceryList.html to update the list total when an item is added to cart, or check off the list

--- proj3/groceryList.js
+++ proj3/groceryList.js
@@ -22,27 +22,10 @@ function addItem(listID, item, price) {
 		listID = "#" + listID;
 	}
 
-	//create a checkbox <input>
-	//<input type="checkbox"/>
-	let checkbox = document.createElement("input");
-
-	checkbox.setAttribute("type", "checkbox");
-	checkbox.addEventListener("change", () => {
-		if (checkbox.nextSibling.classList.contains("itemChecked"))
-			checkbox.nextSibling.classList.remove("itemChecked");
-		else checkbox.nextSibling.classList.add("itemChecked");
-	});
-
 	// create a <span>item name</span>
 	let itemName = document.createElement("span");
 	itemName.setAttribute("id", "itemName");
 	itemName.innerHTML = item;
-
-	// nest the contents inside the list item
-	let div = document.createElement("div");
-	div.setAttribute("class", "checkboxAndName");
-	div.appendChild(checkbox);
-	div.appendChild(itemName);
 
 	// create a <span>item price</span>
 	let itemPrice = document.createElement("span");
@@ -50,6 +33,28 @@ function addItem(listID, item, price) {
 	//format the price of the item
 	let displayPrice = price.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
 	itemPrice.innerHTML = `$${displayPrice}`;
+
+	//create a checkbox <input>
+	//<input type="checkbox"/>
+	let checkbox = document.createElement("input");
+	checkbox.setAttribute("type", "checkbox");
+	checkbox.addEventListener("change", () => {
+		if (checkbox.nextSibling.classList.contains("itemChecked")) {
+			itemName.classList.remove("itemChecked");
+			itemPrice.classList.remove("itemChecked");
+			updateListTotal(price);
+		} else {
+			itemName.classList.add("itemChecked");
+			itemPrice.classList.add("itemChecked");
+			updateListTotal(-price);
+		}
+	});
+
+	// nest the contents inside the list item
+	let div = document.createElement("div");
+	div.setAttribute("class", "checkboxAndName");
+	div.appendChild(checkbox);
+	div.appendChild(itemName);
 
 	// nest the contents inside the list item
 	let newListItem = document.createElement("li");
@@ -62,6 +67,18 @@ function addItem(listID, item, price) {
 	let ul = document.querySelector(listID);
 	let ListTotal = document.querySelector("#total");
 	ul.insertBefore(newListItem, ListTotal);
+
+	//update the total sum of the list
+	updateListTotal(price);
+}
+
+function updateListTotal(delta) {
+	let total = document.querySelector("#totalPrice");
+	//update the value of the new total
+	newTotal = parseFloat(total.innerHTML.substring(1)) + delta;
+	//reformat the new total
+	newTotal = newTotal.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+	total.innerHTML = `$${newTotal}`;
 }
 
 addItem("#groceryList", "Milk", 1.0);
