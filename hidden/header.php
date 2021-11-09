72852146ec54959ffd4a8bdcbbda6ec393e5b6b2
Emilee Oquist <emiwemi495@gmail.com>
2021-11-09 15:34:17 -0800

    header

---
+++ hidden/header.php
@@ -0,0 +1,32 @@
+/*
+ You can use php for making multi-page website.
+
+Create a header.php in which you should put all your html code for menu's and social media etc
+Insert header.php in your index.php using following code
+// within php brackets: php include 'header.php';
+ */
+
+ /*
+ <header class="menu-container">
+    <h1 class="logo">
+        <a class="logo-link" href="./index.html">Grocery App</a>
+    </h1>
+    <nav class="menu">
+        <li class="dropdown">
+            <span>Profile &#9662;</span>
+            <ul class="profile-menu">
+                <!-- Start of submenu -->
+                <li><a class="nav-link" href="./index.html">Index</a></li>
+                <li>
+                    <a class="nav-link" href="proj2/passengerIndex.html">passengerIndex</a>
+                </li>
+                <li><a class="nav-link" href="./projectPlan.html">projectPlan</a></li>
+            </ul>
+            <!-- End of submenu -->
+        </li>
+        <!-- Work on log-in and sign-up data !! also super important to ensure user remains logged in--> 
+        <li><a class="nav-link" href="./index.html">Login</a></li>
+        <li><a class="nav-link" href="./index.html">Sign-Up</a></li>
+    </nav>
+</header>
+ */
