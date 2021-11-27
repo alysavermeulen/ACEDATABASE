cd5941762983b57121506aba692c700362d92de6
Colin Monaghan <cleemonaghan@gmail.com>
2021-11-27 14:19:05 -0800

    Added php to make the content of the grocery list reflect what is in the database

--- GroceryApp/groceryList.php
+++ GroceryApp/groceryList.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="UTF-8" />
 		<title>Index Webpage</title>
-		<link rel="stylesheet" href="groceryApp.css" />
+		<link rel="stylesheet" href="styles.css" />
 	</head>
 
 	<body>
@@ -13,25 +13,9 @@
 					<a class="logo-link" href="./index.html">Grocery App</a>
 				</h1>
 				<nav class="menu">
-					<li class="dropdown">
-						<span>Profile &#9662;</span>
-						<ul class="profile-menu">
-							<!-- Start of submenu -->
-							<li><a class="nav-link" href="./index.html">Thing1</a></li>
-							<li>
-								<a class="nav-link" href="proj2/passengerIndex.html">Thing2</a>
-							</li>
-							<li><a class="nav-link" href="./projectPlan.html">Thing3</a></li>
-						</ul>
-						<!-- End of submenu -->
-					</li>
-					<li><a class="nav-link" href="./index.html">Login</a></li>
-					<li><a class="nav-link" href="./index.html">Sign-Up</a></li>
+					<li><a class="nav-link" href="./groceryLogin.php">Sign Out</a></li>
 				</nav>
 			</header>
-
-			<!-- Temp field for adding items-->
-			<input type="text" name="addItem" placeholder="Add an item to the list" />
 
 			<div>
 				<ul id="groceryList">
@@ -42,10 +26,54 @@
 						<p id="totalPrice">$0.00</p>
 					</li>
 				</ul>
+				
 				<script src="groceryList.js"></script>
+				<?php
+
+					//path to the SQLite database file
+					$db_file = './myDB/grocery.db';
+
+					try {
+						//open connection to the grocery database file
+						$db = new PDO('sqlite:' . $db_file);
+
+						//set errormode to use exceptions
+						$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+						if (!isset($_GET['s'])){
+                            //retrieve food items from all categories
+							$username = 'cmonaghan';
+                            $qry = "select * from UserCart natural join foodItem where username is '".$username."';";
+                            $result_set = $db->query($qry);
+
+
+							// if cart is empty
+							if (empty($result_set)){
+								echo("<br>Cart is empty.");
+							}
+							else{
+								foreach($result_set as $tuple){
+									//addItem("#groceryList", inputNode.value, 1.0);
+									echo '<script type="text/javascript"> 
+									console.log("'.$tuple['foodName'].'");
+									addItem("#groceryList","'.$tuple['foodName'].'", '.$tuple['price'].'); 
+									</script>';
+								}
+
+							}
+						}
+						
+
+						//disconnect from db
+							$db = null;
+					}
+					catch(PDOException $e) {
+						die('Exception : '.$e->getMessage());
+					}
+					?>
+
 			</div>
 			<footer>
-				<p>Authored by: Emilee Oquist, Colin Monaghan, and Alysa Vermeulen.</p>
+				<p>Authored by: Colin Monaghan and Alysa Vermeulen.</p>
 			</footer>
 		</div>
 	</body>
