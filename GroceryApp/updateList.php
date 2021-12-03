f85d5eeecb7e3ee248aa5448466a1b4251b9a842
Colin Monaghan <cleemonaghan@gmail.com>
2021-12-02 20:52:15 -0800

    Created updateList.php to update and delete items in the user's cart

---
+++ GroceryApp/updateList.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+    
+    $username = $_SESSION['username'];
+
+    // path to the SQLite database file
+    $db_file = './myDB/grocery.db';
+
+    try {
+        // open connection to the airport database file
+        $db = new PDO('sqlite:' . $db_file);
+
+        // set errormode to use exceptions
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // prepare to fetch food item info from user cart
+        $qry = "select * from UserCart natural join foodItem where username is '".$username."';";
+        $result_set = $db->query($qry);
+        
+
+        foreach($result_set as $tuple){
+            //get the name of the food in the user's cart
+            $foodName = $tuple['foodName'];
+            $foodID = $tuple['foodID'];
+            //use the name to retrieve the new quantity from the cart
+            $newQuantity = $_POST[$foodName];
+            $oldQuantity = $tuple['quantity'];
+            
+            // if quantity == 0, delete the item from the cart
+            if($newQuantity == 0) {
+                // prepare to delete the item from usercart 
+                $stmt = $db->prepare('DELETE FROM usercart where foodId = ? AND username = ?');
+
+                $stmt->execute([$foodID, $username]);
+            }
+            // otherwise update the cart with the new quantity
+            else if($newQuantity != $oldQuantity) {
+
+                // prepare to update the new quantity into usercart 
+                $stmt = $db->prepare('UPDATE usercart SET quantity = ? WHERE foodId = ? AND username = ?');
+
+                $stmt->execute([$newQuantity, $foodID, $username]);
+            }
+
+
+
+        }
+        // disconnect from db
+        $db = null;
+
+        // redirect to groceryList.php
+        header("Location: groceryList.php");
+        exit();
+    }
+    catch(PDOException $e) {
+        die('Exception : '.$e->getMessage());
+    }
+?>
+
+</body>
+</html>
